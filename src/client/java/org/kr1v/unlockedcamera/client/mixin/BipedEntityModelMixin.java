814d6847d0479a9ff6ddee0d4d158125217a5d75
kr1viah_ <kravpolet@gmail.com>
2025-03-26 11:30:59 +0100

    Fixes #1: don't return null in getHead()

--- src/client/java/org/kr1v/unlockedcamera/client/mixin/BipedEntityModelMixin.java
+++ src/client/java/org/kr1v/unlockedcamera/client/mixin/BipedEntityModelMixin.java
@@ -1,18 +1,14 @@
 package org.kr1v.unlockedcamera.client.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.entity.model.*;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.Arm;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(BipedEntityModel.class)
-public abstract class BipedEntityModelMixin<T extends LivingEntity> implements ModelWithArms, ModelWithHead {
+public abstract class BipedEntityModelMixin implements ModelWithArms, ModelWithHead {
 
     @Unique
     private static final float pi = (float)Math.PI;
@@ -32,22 +28,4 @@ public abstract class BipedEntityModelMixin<T extends LivingEntity> implements M
         else if (normalizedPitch > pi/2) return pi-normalizedPitch;
         return normalizedPitch;
     }
-
-    // functions for extending stuff
-
-    @Override
-    public void setArmAngle(Arm arm, MatrixStack matrices) {
-
-    }
-
-    /**
-     * Gets the head model part.
-     *
-     * @return the head
-     */
-    @Override
-    public ModelPart getHead() {
-        return null;
-    }
-
 }
